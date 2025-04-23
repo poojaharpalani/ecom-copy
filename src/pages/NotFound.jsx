7297959e4348c350bd62347cc8f54a3a7033ffa4
Pooja harplani <harpalanipooja19@gmail.com>
2025-04-23 18:04:49 +0530

    remove similar import in not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
-import React from "react";
 import { Link } from "react-router-dom";
+import React from "react";
+
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
